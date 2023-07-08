84bb9da55401cc5b8441a25537e76762b375320d
Alex <alexgeorgegeer@gmail.com>
2023-07-08 01:33:34 -0500

    done with header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,11 @@ import '../styles/styles.css';
 if (module.hot) {
   module.hot.accept();
 }
+
+// LESSON EXAMPLE CODE BELOW
+function person(name, color) {
+  console.log(`My name is ${name} and my fav color is ${color}`)
+}
+
+person('James Milton', 'pink')
+person('Gisele burke', 'brown')
