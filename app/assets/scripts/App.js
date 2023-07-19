cc2bb651d78d506d0c216b4381ff351e3e4b2607
Alex <alexgeorgegeer@gmail.com>
2023-07-19 16:51:23 -0500

    modal completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,12 +6,26 @@ import NavLinkSections from './modules/NavLinkSections';
 
 new ObserveOnScroll(document.querySelectorAll('.feature-item'), .5);
 new ObserveOnScroll(document.querySelectorAll('.testimonial'), .5);
-
-let stickyHeader = new StickyHeader();
-
-let mobileMenu = new MobileMenu();
-
+new StickyHeader();
+new MobileMenu();
 let navLinkSections = new NavLinkSections();
+
+let modal;
+
+document.querySelectorAll('.open-modal').forEach(el => {
+  el.addEventListener('click', e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */'./modules/Modal').then( (x) => {
+        modal = new x.default();
+        setTimeout(() => modal.openTheModal(), 20);
+      }).catch( () => console.log('Problem'))
+    } else {
+      modal.openTheModal();
+    }
+  })
+});
+
 
 
 if (module.hot) {
